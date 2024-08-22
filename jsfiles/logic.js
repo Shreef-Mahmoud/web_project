428a70ea54f94b04178c0c8f500ad49dccd5259c
Shreef Mahmoud <shefotemo4@gmail.com>
2024-08-22 17:30:35 +0300

    fdf

--- jsfiles/logic.js
+++ jsfiles/logic.js
@@ -1,3 +1,5 @@
+// Shop html Variables
+
 var products = [
     { name: "Manchester City 2024", price: "$49.99", image: "images/product1.jpg" },
     { name: "Real Madrid 2017", price: "$39.99", image: "images/product2.jpg" },
@@ -73,29 +75,41 @@ var products = [
     { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
     { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
     { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
-    { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
-    { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
-    { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
-    { name: "Retro Football Scarf", price: "$24.99", image: "images/product5.jpg" },
-];
+]; // this is the array that contains products data
 
-var currentPage = 0;
-var lastPage = Math.ceil(products.length / 20);
+var currentPage = 0; // this contains the page number minus one
+var lastPage = Math.ceil(products.length / 20); // this is the number of pages calc by the array size
+
+
+window.onscroll = function () { checkScrollPosition() }; // runs the function when page is scrolled
+
+function checkScrollPosition() {
+    var header = document.getElementById("header"); // gets header data
+    var stopSection = document.getElementById("productHeader"); // gets product Header data
+    var stopSectionOffset = stopSection.offsetTop - header.offsetHeight; // tell how far down the product header is
+
+    if (window.pageYOffset >= stopSectionOffset) {
+        header.style.position = "absolute"; // makes the header don't move
+        header.style.top = stopSectionOffset + "px"; // puts the header above product header
+    } else {
+        header.style.position = "fixed"; // make the header move
+        header.style.top = "0"; // puts the header on the top of the website
+    } //checks if the header passed the product header
+}
 
 function shopProducts() {
-    var productShower = document.getElementsByClassName('products')[0];
-    productShower.innerHTML = "";
+    var productShower = document.getElementsByClassName('products')[0]; //gets products data
+    productShower.innerHTML = ""; //empties the html inside
 
-    var pageStart = (currentPage + 1).toString();
-    var textCon = pageStart +  " / " + lastPage.toString();
+    var pageStart = (currentPage + 1).toString(); // turns page number to string
+    var textCon = pageStart + " / " + lastPage.toString(); // make page number counter
 
-    document.getElementById("pageNumber").textContent =  textCon;
+    document.getElementById("pageNumber").textContent = textCon; //adds it to the html
 
-    for (let i = 0 ; i < 20 ; i++)
-        {
-            var product = products[i + (currentPage * 20)];
+    for (let i = 0; i < 20; i++) {
+        var product = products[i + (currentPage * 20)];
 
-            productShower.innerHTML += `
+        productShower.innerHTML += `
                 <div class="product-grid">
                     <img src="${product.image}" alt="${product.name}">
                     <h3>${product.name}</h3>
@@ -103,37 +117,30 @@ function shopProducts() {
                     <button class="btn" onclick="addToCart(${i})">Add to Cart</button>
                 </div>
             `;
-        }
+    } // shows twenty products in grid and sends it to the html
 }
 
-function addToCart(productIndex)
-{
-    confirm(`"${products[productIndex + (currentPage* 20)].name}" added to cart!`);
+function addToCart(productIndex) {
+    confirm(`"${products[productIndex + (currentPage * 20)].name}" added to cart!`);
+    //confirms the payment by saying the bought product
 }
 
-function pageChanger(buttonId)
-{
-    if(document.getElementById(buttonId).value == "Next")
-    {
-        if(currentPage+1 == lastPage)
-        {
+function pageChanger(buttonId) {
+    if (document.getElementById(buttonId).value == "Next") {
+        if (currentPage + 1 == lastPage) {
             alert("It's The Last Page");
         }
-        else
-        {
+        else {
             currentPage++;
-        }
+        } //checks if its the last page to send alert or add a page
     }
-    else if(document.getElementById(buttonId).value == "Back")
-    {
-        if(currentPage == 0)
-        {
+    else if (document.getElementById(buttonId).value == "Back") {
+        if (currentPage == 0) {
             alert("It's The First Page");
         }
-        else
-        {
+        else {
             currentPage--;
-        }
+        } //checks if its the first page to send alert or remove a page
     }
-    shopProducts();
+    shopProducts(); //shows the new products to the user
 }
